b8d444bd14e08810ccd12f67bd946035072b685a
TadhgShinkwin <tadhgcode@gmail.com>
2020-11-03 00:00:36 +0000

    some quick edits with useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
+import React, {useState} from "react"
 import Input from "./components/Input"
 import List from "./components/List"
 
 function App() {
+  const [ todo, setTodo ] = useState("walk dog")
+
+  function changeState(){
+    setTodo("go home")
+  }
+
   return (
     <div>
-      <h1>This is the main app now</h1>
+      <h1>This is the main app now - {todo}</h1>
       <Input />
       <List />
+      <button onClick={changeState}></button>
     </div>
   );
 }
