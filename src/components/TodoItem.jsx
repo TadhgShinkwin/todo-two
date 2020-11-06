1af7d28d03396237ebbe59e765b3c4b0a2a95da2
TadhgShinkwin <tadhgcode@gmail.com>
2020-11-06 17:18:11 +0000

    Formatting changes for complete/incomplete

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,11 +3,20 @@ import React from "react"
 
  function TodoItem({todo, index, completeTodo, removeTodo, chooseColor}){
      return(
-         <div style={{backgroundColor: todo.color ? todo.color: "", textDecoration: todo.isCompleted ? "line-through":""}} className="todo">
+         <div 
+         style={{
+                backgroundColor: todo.color ? todo.color: "", 
+                textDecoration: todo.isCompleted ? "line-through":"", 
+                fontStyle: todo.isCompleted ? "italic": "",
+                color: todo.isCompleted ? "#8c8c8c": ""
+        }} 
+         className="todo"
+         >
              {todo.text}
              <div>
                  <button onClick={()=>completeTodo(index)}>&#10004;</button>
                  <button onClick={()=>removeTodo(index)}>X</button>
+                 <br/>
                  <select name="colorChoose" value={todo.color} onChange={e=>chooseColor(index,e.target.value)}>
                      <option value="" disabled selected>--Set Difficulty--</option>
                      <option value="#99ffbb">Easy</option>
